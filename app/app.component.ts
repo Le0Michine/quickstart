d0dbcf1870e86f007b243bd4b7b2a9930800d07a
Leo <leomichine@gmail.com>
2016-10-09 22:01:28 +0200

    init

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+        <h1>My First Angular component</h1>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
+  providers: [  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
